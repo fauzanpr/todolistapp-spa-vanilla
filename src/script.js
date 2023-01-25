38377d838cfc704a1e86f6ace0e034614a845b7d
fauzanpr <fauzanpr.06@gmail.com>
2023-01-25 18:42:47 +0700

    create homepage and todo components

--- src/script.js
+++ src/script.js
@@ -1,2 +1,34 @@
 'use strict';
 
+function HomePage() {
+    const homePage = document.createElement('div');
+    const addTodoListInput = document.createElement('input');
+    const btnTodoList = document.createElement('button');
+    const todolistContainer = document.createElement('ul');
+    todolistContainer.append(Todo({
+        content: 'Todo 1'
+    }));
+    todolistContainer.append(Todo({
+        content: 'Todo 2'
+    }));
+    btnTodoList.textContent = 'Add';
+    homePage.append(addTodoListInput);
+    homePage.append(btnTodoList);
+    homePage.append(todolistContainer);
+    return homePage;
+}
+
+function Todo(props) {
+    const todolist = document.createElement('li');
+    todolist.textContent = props.content;
+    return todolist;
+}
+
+function render() {
+    const root = document.getElementById('root');
+    const homePage = HomePage();
+    root.textContent = '';
+    root.append(homePage);
+}
+
+render();
