19aedfa56d00f338891f430931cfca3371a24c13
fauzanpr <fauzanpr.06@gmail.com>
2023-02-01 14:03:55 +0700

    add todolist using state

--- script.js
+++ script.js
@@ -1,49 +1,59 @@
-'use strict';
+"use strict";
 
 let state = {
-    todo: [],
+  todo: [],
 };
 
 function setState(newState) {
-    state = {...state, ...newState};
+  if (newState.todo) {
+    state.todo.push(newState.todo);
+  } else {
+    state = { ...state, ...newState };
+  }
+  render();
 }
 
-function HomePage() {
-    const homePage = document.createElement('div');
-    const addTodoListInput = document.createElement('input');
-    const btnTodoList = document.createElement('button');
-    const todolistContainer = document.createElement('ul');
+function onStateChange(prevState, nextState) {}
 
-    btnTodoList.onclick = function(event) {
-        if (addTodoListInput.value) {
-            setState({
-                todo: [...state.todo, addTodoListInput.value],
-            });
-            addTodoListInput.value = '';
-        }
+function HomePage() {
+  const homePage = document.createElement("div");
+  const addTodoListInput = document.createElement("input");
+  const btnTodoList = document.createElement("button");
+  const todolistContainer = document.createElement("ul");
+
+  btnTodoList.onclick = function () {
+    if (addTodoListInput.value) {
+      setState({ todo: addTodoListInput.value });
+      addTodoListInput.value = "";
     }
-    btnTodoList.textContent = 'Add';
-    homePage.append(addTodoListInput);
-    homePage.append(btnTodoList);
-    homePage.append(todolistContainer);
-    return homePage;
+  };
+  if (state.todo.length > 0) {
+    for (const [i, todo] of state.todo.entries()) {
+      todolistContainer.append(Todo({ content: todo, id: i }));
+    }
+  }
+  btnTodoList.textContent = "Add";
+  homePage.append(addTodoListInput);
+  homePage.append(btnTodoList);
+  homePage.append(todolistContainer);
+  return homePage;
 }
 
 function Todo(props) {
-    const todolist = document.createElement('li');
-    todolist.textContent = props.content;
-    const btnDelete = document.createElement('button');
-    btnDelete.textContent = `${props.id}`;
-    btnDelete.id = `${props.id}`;
-    todolist.append(btnDelete);
-    return todolist;
+  const todolist = document.createElement("li");
+  todolist.textContent = props.content;
+  const btnDelete = document.createElement("button");
+  btnDelete.textContent = `${props.id}`;
+  btnDelete.id = `${props.id}`;
+  todolist.append(btnDelete);
+  return todolist;
 }
 
 function render() {
-    const root = document.getElementById('root');
-    const homePage = HomePage();
-    root.textContent = '';
-    root.append(homePage);
+  const root = document.getElementById("root");
+  const homePage = HomePage();
+  root.textContent = "";
+  root.append(homePage);
 }
 
 render();
